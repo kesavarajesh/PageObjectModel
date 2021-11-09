1aeb599edb7e1c9ddf8176e9c38932ae776319d4
kesavarajesh <kesava.rajesh@gmail.com>
2021-11-09 09:47:47 -0600

    modified the path

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,11 +41,11 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");	
+			System.setProperty("webdriver.chrome.driver", "C://Tools//ChromeDriver//chromedriver");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
-			System.setProperty("webdriver.gecko.driver", "/Users/naveenkhunteta/Documents/SeleniumServer/geckodriver");	
+			System.setProperty("webdriver.gecko.driver", "C://Tools//FFDriver//geckodriver");	
 			driver = new FirefoxDriver(); 
 		}
 		
@@ -66,11 +66,4 @@ public class TestBase {
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-
 }
